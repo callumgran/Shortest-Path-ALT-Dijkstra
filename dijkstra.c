18eedd0d305914713344142da74c16d34bfb1783
Callum Gran <callum.gran@gmail.com>
2022-11-08 20:00:15 +0100

    added functionality to read doubles

--- dijkstra.c
+++ dijkstra.c
@@ -6,11 +6,6 @@
 
 typedef bool (compare_func)(void *a, void *b);
 
-struct position {
-    double longitude;
-    double latitude;
-};
-
 struct heapq_t {
     void            **items; 
     size_t          size;
@@ -21,8 +16,9 @@ struct heapq_t {
 struct edge_t {
     uint32_t        from_idx;
     uint32_t        to_idx;
+    uint32_t        m_dist;
     uint32_t        t_dist;
-    uint32_t        t_dist;
+    uint32_t        spd_lim;
     struct edge_t   *next;
 };
 
@@ -32,15 +28,28 @@ struct graph_t {
     size_t          edge_count;
 };
 
+struct position_t {
+    double          longitude;
+    double          latitude;
+};
+
 struct node_info_t {
     uint32_t        node_idx;
-    uint32_t        cost_from_start_node;
-    struct position *position;
+    uint32_t        t_tot;
+    uint32_t        m_tot;
+    struct position_t *position;
+};
+
+struct node_list_t {
+    size_t          len;
+    struct node_info_t* nodes;
 };
 
 struct shortest_path_t {
-    uint32_t        previous_idx;
-    uint32_t        total_cost;
+    uint32_t        prev_idx;
+    uint32_t        prev_tot;
+    uint32_t        m_tot;
+    uint32_t        t_tot;
 };
 
 #define HEAPQ_STARTING_CAPACITY 32
@@ -143,11 +152,11 @@ void heapq_push(struct heapq_t *hq, void *item)
     heapify_up(hq);
 }
 
-void heapq_push_node(struct heapq_t *hq, int node, int cost_from_start_node)
+void heapq_push_node(struct heapq_t *hq, int node, int m_tot)
 {
     struct node_info_t *ni = malloc(sizeof(struct node_info_t));
     ni->node_idx = node;
-    ni->cost_from_start_node = cost_from_start_node;
+    ni->m_tot = m_tot;
     heapq_push(hq, ni);
 }
 
@@ -169,8 +178,8 @@ struct heapq_t *heapq_malloc(compare_func *cmp)
 
 bool compare(void *a, void *b)
 {
-    return ((struct node_info_t *)a)->cost_from_start_node >
-           ((struct node_info_t *)b)->cost_from_start_node;
+    return ((struct node_info_t *)a)->m_tot >
+           ((struct node_info_t *)b)->m_tot;
 }
 
 void graph_print(struct graph_t *graph)
@@ -183,7 +192,7 @@ void graph_print(struct graph_t *graph)
         printf("[%d] ", i);
         edge_i = graph->neighbour_list[i];
         while (edge_i != NULL) {
-            printf("(%d, %d) ", edge_i->to_idx, edge_i->dist_m);
+            printf("(%d, %d, %d) ", edge_i->to_idx, edge_i->m_dist, edge_i->t_dist);
             edge_i = edge_i->next;
         }
         putchar('\n');
@@ -228,13 +237,116 @@ static int f_next_int(FILE *fp)
     return atoi(buf);
 }
 
-static void graph_insert(struct graph_t *graph, int from_idx, int to_idx, int dist_m, int dist_t)
+static double f_next_double(FILE *fp)
+{
+    char    ch;
+    char    *strtodp;
+    char    buf[128] = {0};
+    int     i;
+
+    i = 0;
+
+    while ((ch = fgetc(fp)) == ' ') {
+    }
+
+    if (ch == EOF)
+        return ERROR_EOF;
+
+    buf[i++] = ch;
+
+    do {
+        ch = fgetc(fp);
+        if (ch == ' ') {
+            buf[i] = 0;
+            break;
+        } else if (ch == '\n') {
+            break;
+        } else if (i >= 128) {
+            fprintf(stderr, "Error parsing file: node longer than 128 chars\n");
+            exit(1);
+            break;
+        }
+
+        buf[i++] = ch;
+    } while (ch != EOF);
+
+    if (ch == EOF)
+        return ERROR_EOF;
+
+    return strtod(buf, &strtodp);
+}
+
+static void node_list_insert(struct node_list_t *nl, uint32_t n_idx, 
+                                double latitude, double longitude)
+{
+    struct node_info_t *n_info;
+    struct position_t *p;
+
+    n_info = (struct node_info_t *)(malloc(sizeof(struct node_info_t)));
+    p = (struct position_t *)(malloc(sizeof(struct position_t)));
+
+    p->latitude = latitude;
+    p->longitude = longitude;
+
+    n_info->node_idx = n_idx;
+    n_info->m_tot = 0;
+    n_info->t_tot = 0;
+    n_info->position = p;
+
+    nl->nodes[n_idx] = *n_info;
+
+    free(n_info);
+}
+
+struct node_list_t *parse_nodes(char *file_name)
+{
+    FILE                *fp;
+    size_t              node_count;
+    size_t              i;
+    int                 n_idx;
+    double              latitude;
+    double              longitude;
+    char                ch;
+    struct node_list_t  *nl;
+
+    fp = fopen(file_name, "r");
+    if (fp == NULL)
+        return NULL;
+
+    node_count = f_next_int(fp);
+
+    nl = (struct node_list_t *)(malloc(sizeof(struct node_list_t)));
+
+    nl->len = node_count;
+
+    nl->nodes = (struct node_info_t *)
+                    (calloc(nl->len, sizeof(struct node_info_t)));
+
+    do {
+        n_idx = f_next_int(fp);
+        latitude = f_next_double(fp);
+        longitude = f_next_double(fp);
+            
+        if (n_idx == ERROR_EOF || latitude == (double)ERROR_EOF || longitude == (double)ERROR_EOF)
+            break;
+
+        node_list_insert(nl, n_idx, latitude, longitude);
+    } while (1);
+
+    fclose(fp);
+
+    return nl;
+}
+
+static void graph_insert(struct graph_t *graph, uint32_t from_idx, uint32_t to_idx, 
+                                uint32_t m_dist, uint32_t t_dist, uint32_t spd_lim)
 {
     struct edge_t *edge = malloc(sizeof(struct edge_t));
     edge->from_idx = from_idx;
     edge->to_idx = to_idx;
-    edge->dist_m = dist_m;
-    edge->dist_t = dist_t;
+    edge->m_dist = m_dist;
+    edge->t_dist = t_dist;
+    edge->spd_lim = spd_lim;
     edge->next = NULL;
 
     struct edge_t *head = graph->neighbour_list[from_idx];
@@ -247,69 +359,46 @@ static void graph_insert(struct graph_t *graph, int from_idx, int to_idx, int di
     graph->neighbour_list[from_idx] = edge;
 }
 
-struct graph_t *parse_nodes_into_graph(char *file_name)
+struct graph_t *parse_edges_into_graph(char *file_name, size_t node_count)
 {
-    FILE *fp;
+    FILE        *fp;
+    int         from_idx;
+    int         to_idx;
+    int         t_dist;
+    int         m_dist;
+    int         spd_lim;
+    int         edge_count;
+    char        ch;
+    struct  graph_t *graph;
+
     fp = fopen(file_name, "r");
     if (fp == NULL)
         return NULL;
 
-    char ch;
-    struct graph_t *graph = malloc(sizeof(struct graph_t));
+    graph = malloc(sizeof(struct graph_t));
 
-    int node_count = f_next_int(fp);
-    int edge_count = f_next_int(fp);
+    edge_count = f_next_int(fp);
 
-    graph->neighbour_list = malloc(sizeof(struct edge_t *) * node_count);
-    
-    for (int i = 0; i < node_count; i++)
-        graph->neighbour_list[i] = NULL;
+    graph->neighbour_list = (struct edge_t *)(calloc(node_count, sizeof(struct edge_t *)));
+
 
     do {
-        int from_idx = f_next_int(fp);
-        int to_idx = f_next_int(fp);
-        int dist_m = f_next_int(fp);
-        int dist_t = f_next_int(fp);
-        int spd_lim = f_next_int(fp);
-        if (from_idx == ERROR_EOF || to_idx == ERROR_EOF || cost == ERROR_EOF)
+        from_idx = f_next_int(fp);
+        to_idx = f_next_int(fp);
+        t_dist = f_next_int(fp);
+        m_dist = f_next_int(fp);
+        spd_lim = f_next_int(fp);
+
+        if (from_idx == ERROR_EOF || to_idx == ERROR_EOF 
+            || t_dist == ERROR_EOF || m_dist == ERROR_EOF 
+            || spd_lim == ERROR_EOF)
             break;
-        graph_insert(graph, from_idx, to_idx, cost);
+        
+        graph_insert(graph, from_idx, to_idx, t_dist, m_dist, spd_lim);
     } while (1);
 
     fclose(fp);
-    graph->node_count = node_count;
-    graph->edge_count = edge_count;
-    return graph;
-}
 
-struct graph_t *parse_edges_into_graph(char *file_name)
-{
-    FILE *fp;
-    fp = fopen(file_name, "r");
-    if (fp == NULL)
-        return NULL;
-
-    char ch;
-    struct graph_t *graph = malloc(sizeof(struct graph_t));
-
-    int node_count = f_next_int(fp);
-    int edge_count = f_next_int(fp);
-
-    graph->neighbour_list = malloc(sizeof(struct edge_t *) * node_count);
-    
-    for (int i = 0; i < node_count; i++)
-        graph->neighbour_list[i] = NULL;
-
-    while (1) {
-        int from_idx = f_next_int(fp);
-        int to_idx = f_next_int(fp);
-        int cost = f_next_int(fp);
-        if (from_idx == ERROR_EOF || to_idx == ERROR_EOF || cost == ERROR_EOF)
-            break;
-        graph_insert(graph, from_idx, to_idx, cost);
-    }
-
-    fclose(fp);
     graph->node_count = node_count;
     graph->edge_count = edge_count;
     return graph;
@@ -318,8 +407,11 @@ struct graph_t *parse_edges_into_graph(char *file_name)
 
 void graph_free(struct graph_t *graph)
 {
-    struct edge_t *edge_i, *prev;
-    for (int i = 0; i < graph->node_count; i++) {
+    struct edge_t   *edge_i, 
+                    *prev;
+    int             i;
+
+    for (i = 0; i < graph->node_count; i++) {
         edge_i = graph->neighbour_list[i];
         while (edge_i != NULL) {
             prev = edge_i;
@@ -327,47 +419,52 @@ void graph_free(struct graph_t *graph)
             free(prev);
         }
     }
+
     free(graph->neighbour_list);
     free(graph);
 }
 
 void shortest_path_t_print(struct shortest_path_t *sp, int node_count)
 {
+    int         i;
+
     printf("[node] [previous node] [cost]\n");
     printf("-----------------------------------\n");
-    for (int i = 0; i < node_count; i++) {
+
+    for (i = 0; i < node_count; i++) {
         printf("%d\t", i);
-        if (sp[i].previous_idx == NODE_START) {
+        if (sp[i].prev_idx == NODE_START) {
             printf("start\n");
             continue;
-        } else if (sp[i].previous_idx == NODE_UNVISITED) {
+        } else if (sp[i].prev_idx == NODE_UNVISITED) {
             printf("unreachable\n");
             continue;
         } else {
-            printf("%d\t", sp[i].previous_idx);
+            printf("%d\t", sp[i].prev_idx);
         }
 
-        if (sp[i].total_cost == INF)
-            printf("ERROR: visited node has total_cost set to infinity\n");
+        if (sp[i].m_tot == INF)
+            printf("ERROR: visited node has m_tot set to infinity\n");
         else
-            printf("%d\n", sp[i].total_cost);
+            printf("%d\n", sp[i].m_tot);
     }
 }
 
-struct shortest_path_t *dijkstra(struct graph_t *graph, int start_node)
+struct shortest_path_t *dijkstra(struct graph_t *graph, uint32_t start_node)
 {
     bool visited[graph->node_count];
+    
     memset(visited, false, sizeof(bool) * graph->node_count);
     struct heapq_t *hq = heapq_malloc(compare);
     struct shortest_path_t *sp = malloc(sizeof(struct shortest_path_t) * graph->node_count);
 
     for (int i = 0; i < graph->node_count; i++) {
-        sp[i].total_cost = INF;
-        sp[i].previous_idx = NODE_UNVISITED;
+        sp[i].m_tot = INF;
+        sp[i].prev_idx = NODE_UNVISITED;
     }
 
-    sp[start_node].total_cost = 0;
-    sp[start_node].previous_idx = NODE_START;
+    sp[start_node].m_tot = 0;
+    sp[start_node].prev_idx = NODE_START;
     heapq_push_node(hq, start_node, 0);
 
     struct edge_t *neighbour;
@@ -381,10 +478,10 @@ struct shortest_path_t *dijkstra(struct graph_t *graph, int start_node)
                 neighbour = neighbour->next;
                 continue;
             }
-            int new_cost = sp[ni->node_idx].total_cost + neighbour->dist_m;
-            if (new_cost < sp[neighbour->to_idx].total_cost) {
-                sp[neighbour->to_idx].previous_idx = ni->node_idx;
-                sp[neighbour->to_idx].total_cost = new_cost;
+            int new_cost = sp[ni->node_idx].m_tot + neighbour->m_dist;
+            if (new_cost < sp[neighbour->to_idx].m_tot) {
+                sp[neighbour->to_idx].prev_idx = ni->node_idx;
+                sp[neighbour->to_idx].m_tot = new_cost;
                 heapq_push_node(hq, neighbour->to_idx, new_cost);
             }
             neighbour = neighbour->next;
@@ -399,8 +496,9 @@ struct shortest_path_t *dijkstra(struct graph_t *graph, int start_node)
 
 void do_dijkstra(char *file_name, int starting_node)
 {
+    struct node_list_t *nl = parse_nodes_info();
     struct shortest_path_t *sp;
-    struct graph_t *graph = parse_file_into_graph(file_name);
+    struct graph_t *graph = parse_file_into_graph(file_name, nl->len);
     if (graph == NULL) {
         fprintf(stderr, "error: could not read file '%s'\n", file_name);
         exit(1);
