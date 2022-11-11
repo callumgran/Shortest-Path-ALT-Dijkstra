b1ca377f854b059ad3ec8118cf7289751463059b
Nicolai Brand <nicolahb@stud.ntnu.no>
2022-11-11 18:39:00 +0100

    fix file open

--- alt_preprocess.c
+++ alt_preprocess.c
@@ -15,11 +15,18 @@ pthread_cond_t t_cond = PTHREAD_COND_INITIALIZER;
 static void write_distances_to_file(struct shortest_path *sp, struct dijkstra_t *d)
 {
     FILE *output;
-    output = fopen(d->name, "r");
+    output = fopen(d->name, "w");
+
+    if (output == NULL) {
+        fprintf(stderr, "could not open file :(");
+        exit(1);
+    }
+        
 
     for (int i = 0; i < d->graph->node_count; i++)
         fwrite(&sp[i].total_cost, 4, 1, output);
     
+    printf("done\n");
     fclose(output);
 }
 
@@ -65,12 +72,18 @@ void preprocess(char *node_file, char *edge_file)
     int landmarks[3] = {LANDMARK_ONE, 
                         LANDMARK_TWO, 
                         LANDMARK_THREE};
-    char output_files[6][8] = {"1cor.txt",
-                                "1rev.txt",
-                                "2cor.txt",
-                                "2rev.txt",
-                                "3cor.txt",
-                                "3rev.txt",};
+
+
+    char **output_files = malloc(sizeof(char *) * 6);
+    for (int i = 0; i < 6; i++)
+        output_files[i] = malloc(sizeof(char) * 32);
+
+    strncpy(output_files[0], "1rev.txt", 32);
+    strncpy(output_files[1], "2cor.txt", 32);
+    strncpy(output_files[2], "2rev.txt", 32);
+    strncpy(output_files[3], "2cor.txt", 32);
+    strncpy(output_files[4], "3rev.txt", 32);
+    strncpy(output_files[5], "3cor.txt", 32);
     
     n_threads = 0;
     int j = 0;
@@ -80,11 +93,11 @@ void preprocess(char *node_file, char *edge_file)
 
         parse_node_file(node_file, &graph_correct);
         parse_edge_file(edge_file, &graph_correct, false);
-        create_thread(&graph_reversed, landmarks[i], strdup(output_files[j++]));
+        create_thread(&graph_correct, landmarks[i], output_files[j++]);
 
         parse_node_file(node_file, &graph_reversed);
         parse_edge_file(edge_file, &graph_reversed, true);
-        create_thread(&graph_reversed, landmarks[i], strdup(output_files[j++]));
+        create_thread(&graph_reversed, landmarks[i], output_files[j++]);
     }
     
     pthread_mutex_lock(&t_mutex);
